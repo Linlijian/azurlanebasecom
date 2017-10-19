5c1eebff5d8cc6ca534962f1ca4b226b0d15fa22
Linlijian <Praw__Rul2__lvluk@outlook.co.th>
2017-10-19 23:12:09 +0700

    Finish insert charecter demo not fix.

--- index.php
+++ index.php
@@ -20,8 +20,8 @@ $result = $conn->query($sql);
 
 
 
- 
-if ($result->num_rows > 0) {   
+
+if ($result->num_rows > 0) {
  while($row = $result->fetch_assoc()){
        echo "id: " .$row["id"]."eff: " . $row["eff"]. "<br>";
     }
@@ -29,14 +29,14 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
- 
+
 
 
 
 $sql = "SELECT picture FROM charecter where id=1";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {   
+if ($result->num_rows > 0) {
  while($row = $result->fetch_assoc()){
         $pic=$row["picture"];
     }
@@ -44,12 +44,20 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
-
+echo "NULL,".$i." 'ch'".$i.".png";
+//NULL, '6', 'ch6.png', 'ch6', 'ch6', 'ch6', 'ch6', 'ch6', 'ch6'), (NULL, '7', 'ch7.png', 'ch7', 'ch7', 'ch7', 'ch7', 'ch7', 'ch7');
+echo "NSERT INTO `charecter` (`id`, `charectorID`, `picture`, `status`, `beginStatus`, `skill`, `introduce`, `reModel`, `cusTom`) VALUES (";
+$i = 8;
 while($i!=311){
-echo "ch".$i.':'.'<br>';
-echo "sub-ch".$i.':'.'<br>';
+echo "NULL,'".$i."'"." ,'ch".$i.".png'".",'ch".$i."'".",'ch".$i."'".",'ch".$i."'".",'ch".$i."'".",'ch".$i."'".",'ch".$i."'),".'<br>(';
 $i++;
 }
+
+
+
+
+
+
 ?>
 
 <img src="img/sub-<?php echo $pic;?>">
